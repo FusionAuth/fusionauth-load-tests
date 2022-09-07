0675d8b425c90f7de9152bb0fc922f07b3f4e534
Daniel DeGroff <daniel@fusionauth.io>
2022-09-07 13:13:14 -0600

    Copyright

--- src/main/script/load-test.sh
+++ src/main/script/load-test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #
-# Copyright (c) 2022-2022, FusionAuth, All Rights Reserved
+# Copyright (c) 2022, FusionAuth, All Rights Reserved
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
