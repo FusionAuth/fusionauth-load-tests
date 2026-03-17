3aae02afafd7294eeec1383e023016a5bd8e68c3
Brent Halsey <211656+bhalsey@users.noreply.github.com>
2026-03-17 07:04:20 -0600

    Bhalsey/eng 3965 reproduce patch race (#32)
    * add PatchIdentityProviderWorker
    * add idp put load test

---
+++ src/main/java/io/fusionauth/load/FusionAuthPatchIdentityProviderWorker.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2012-2025, FusionAuth, All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+package io.fusionauth.load;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.inversoft.error.Errors;
+import com.inversoft.rest.ClientResponse;
+import io.fusionauth.client.FusionAuthClient;
+import io.fusionauth.domain.api.IdentityProviderResponse;
+
+/**
+ * Makes a PATCH /api/identity-provider/{idpId} call, enabling the identity provider for
+ * the application corresponding to the counter.
+ * <br/>
+ * You likely want to reset the idp after a load test to clear the enabled applications.
+ * Counting the applications provides a count of successful calls.
+ * E.g.,
+ * <pre>
+ *   curl -s https://local.fusionauth.io/api/identity-provider/{idpId} -H "Authorization: {api_key}" | jq '.identityProvider.applicationConfiguration | length'
+ * </pre>
+ *
+ * @author Brent Halsey
+ */
+public class FusionAuthPatchIdentityProviderWorker extends FusionAuthBaseWorker {
+  private final AtomicInteger counter;
+
+  private final UUID idpId;
+
+  private final int maxAttempts;
+
+  public FusionAuthPatchIdentityProviderWorker(FusionAuthClient client, Configuration configuration, AtomicInteger counter) {
+    super(client, configuration);
+    this.counter = counter;
+    if (!configuration.hasProperty("idpId")) {
+      throw new IllegalArgumentException("The 'idpId' configuration property is required for FusionAuthPatchIdentityProviderWorker.");
+    }
+    String idpIdString = configuration.getString("idpId");
+    try {
+      this.idpId = UUID.fromString(idpIdString);
+    } catch (IllegalArgumentException e) {
+      throw new IllegalArgumentException("The 'idpId' configuration property must be a valid UUID. Value was: " + idpIdString, e);
+    }
+    this.maxAttempts = configuration.getInteger("maxAttempts", 10);
+  }
+
+  @Override
+  public boolean execute() {
+    setApplicationIndex(counter.incrementAndGet());
+
+    Map<String, Object> patch = Map.of("identityProvider",
+                                           Map.of("applicationConfiguration",
+                                                  Map.of(applicationId.toString(),
+                                                         Map.of("createRegistration", Boolean.TRUE, "enabled", Boolean.TRUE)),
+                                           "tenantConfiguration",
+                                                  Map.of(tenantId.toString(),
+                                                         Map.of("limitUserLinkCount",
+                                                                Map.of("maximumLinks", 42, "enabled", Boolean.TRUE)))));
+    ClientResponse<IdentityProviderResponse, Errors> result = retryablePatch(idpId, patch);
+    if (result.wasSuccessful()) {
+      return true;
+    }
+
+    printErrors(result);
+    return false;
+  }
+
+  private ClientResponse<IdentityProviderResponse, Errors> retryablePatch(UUID idpId, Map<String, Object> patch) {
+    return retryablePatch(idpId, patch, 1);
+  }
+
+  // Do our own retry logic (until we add retry support in the java client)
+  private ClientResponse<IdentityProviderResponse, Errors> retryablePatch(UUID idpId, Map<String, Object> patch, int attempt) {
+    ClientResponse<IdentityProviderResponse, Errors> result = client.patchIdentityProvider(idpId, patch);
+    if (result.wasSuccessful() || attempt == maxAttempts) {
+      return result;
+    } else if (result.status == 409) {
+      try {
+        long backoff = (long) (500 * Math.pow(1.5, attempt));
+        long jitter = (long) (Math.random() * 0.10 * backoff);
+        if (debug) {
+          System.out.printf("Got 409 on attempt %d, sleeping for %d ms + %d ms and retrying\n", attempt, backoff, jitter);
+        }
+        Thread.sleep(backoff + jitter);
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+        return result;
+      }
+      return retryablePatch(idpId, patch, attempt + 1);
+    } else {
+      return result;
+    }
+  }
+}
---
+++ src/main/java/io/fusionauth/load/FusionAuthPutIdentityProviderWorker.java
@@ -0,0 +1,153 @@
+/*
+ * Copyright (c) 2012-2025, FusionAuth, All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+package io.fusionauth.load;
+
+import java.net.URI;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import com.inversoft.error.Errors;
+import com.inversoft.rest.ClientResponse;
+import io.fusionauth.client.FusionAuthClient;
+import io.fusionauth.domain.api.IdentityProviderRequest;
+import io.fusionauth.domain.api.IdentityProviderResponse;
+import io.fusionauth.domain.provider.IdentityProviderLimitUserLinkingPolicy;
+import io.fusionauth.domain.provider.IdentityProviderLinkingStrategy;
+import io.fusionauth.domain.provider.IdentityProviderOauth2Configuration;
+import io.fusionauth.domain.provider.IdentityProviderTenantConfiguration;
+import io.fusionauth.domain.provider.OpenIdConnectApplicationConfiguration;
+import io.fusionauth.domain.provider.OpenIdConnectIdentityProvider;
+
+/**
+ * Makes a PUT /api/identity-provider/{idpId} call, enabling the identity provider for
+ * a random selection of applications and tenants.
+ * Useful for finding race conditions (like DB deadlock).
+ *
+ * @author Brent Halsey
+ */
+public class FusionAuthPutIdentityProviderWorker extends FusionAuthBaseWorker {
+  private final AtomicInteger counter;
+
+  private final UUID idpId;
+
+  private final int maxAttempts;
+
+  private final int numEnabledApplications;
+
+  private final int numEnabledTenants;
+
+  public FusionAuthPutIdentityProviderWorker(FusionAuthClient client, Configuration configuration, AtomicInteger counter) {
+    super(client, configuration);
+    this.counter = counter;
+    if (!configuration.hasProperty("idpId")) {
+      throw new IllegalArgumentException("Configuration property 'idpId' is required for FusionAuthPutIdentityProviderWorker.");
+    }
+    this.idpId = UUID.fromString(configuration.getString("idpId"));
+
+    if (configuration.hasProperty("numEnabledApplications")) {
+      this.numEnabledApplications = configuration.getInteger("numEnabledApplications");
+    } else {
+      this.numEnabledApplications = Integer.min(10, applicationCount);
+    }
+
+    if (configuration.hasProperty("numEnabledTenants")) {
+      this.numEnabledTenants = configuration.getInteger("numEnabledTenants");
+    } else {
+      this.numEnabledTenants = Integer.min(10, tenantCount);
+    }
+
+    this.maxAttempts = configuration.getInteger("maxAttempts", 10);
+  }
+
+  @Override
+  public boolean execute() {
+
+    // build application configs for a random selection of applications
+    List<Integer> allApps = IntStream.rangeClosed(1, applicationCount).boxed().collect(Collectors.toList());
+    Collections.shuffle(allApps);
+    int enabledApplications = Math.min(numEnabledApplications, applicationCount);
+    Map<UUID, OpenIdConnectApplicationConfiguration> appConfig = new HashMap<>();
+    for (int i = 0; i < enabledApplications; i++) {
+      appConfig.put(applicationUUID(allApps.get(i)), new OpenIdConnectApplicationConfiguration().with(a -> a.enabled = true));
+    }
+
+    // build tenant configs for a random selection of tenants
+    List<Integer> allTenants = IntStream.rangeClosed(1, tenantCount).boxed().collect(Collectors.toList());
+    Collections.shuffle(allTenants);
+    int enabledTenants = Math.min(numEnabledTenants, tenantCount);
+    Map<UUID, IdentityProviderTenantConfiguration> tenantConfig = new HashMap<>();
+    for (int i = 0; i < enabledTenants; i++) {
+      tenantConfig.put(tenantUUID(allTenants.get(i)), new IdentityProviderTenantConfiguration()
+          .with(t -> t.limitUserLinkCount = new IdentityProviderLimitUserLinkingPolicy()
+              .with(p -> p.enabled = true)));
+    }
+
+    OpenIdConnectIdentityProvider idp = new OpenIdConnectIdentityProvider()
+        .with(i -> i.name = "load-test-oidc-idp")
+        .with(i -> i.enabled = true)
+        .with(i -> i.linkingStrategy = IdentityProviderLinkingStrategy.LinkByEmail)
+        .with(i -> i.applicationConfiguration = appConfig)
+        .with(i -> i.tenantConfiguration = tenantConfig)
+        .with(i -> i.oauth2 = new IdentityProviderOauth2Configuration()
+            .with(o -> o.client_id = "client_id")
+            .with(o -> o.client_secret = "client_secret")
+            .with(o -> o.authorization_endpoint = URI.create("https://idp.fusionauth.io/p/oauth2/authorize"))
+            .with(o -> o.token_endpoint = URI.create("https://idp.fusionauth.io/p/oauth2/token"))
+            .with(o -> o.userinfo_endpoint = URI.create("https://idp.fusionauth.io/p/oauth2/userinfo"))
+            .with(o -> o.emailClaim = "email"));
+
+    ClientResponse<IdentityProviderResponse, Errors> result = retryablePut(idpId, new IdentityProviderRequest(idp));
+    if (result.wasSuccessful()) {
+      return true;
+    }
+
+    printErrors(result);
+    return false;
+  }
+
+  private ClientResponse<IdentityProviderResponse, Errors> retryablePut(UUID idpId, IdentityProviderRequest idpRequest) {
+    return retryablePut(idpId, idpRequest, 1);
+  }
+
+  // Do our own retry logic (until we add retry support in the java client)
+  private ClientResponse<IdentityProviderResponse, Errors> retryablePut(UUID idpId, IdentityProviderRequest idpRequest, int attempt) {
+    ClientResponse<IdentityProviderResponse, Errors> result = client.updateIdentityProvider(idpId, idpRequest);
+    if (result.wasSuccessful() || attempt == maxAttempts) {
+      return result;
+    } else if (result.status == 409) {
+      try {
+        long backoff = (long) (500 * Math.pow(1.5, attempt));
+        long jitter = (long) (Math.random() * 0.10 * backoff);
+        if (debug) {
+          System.out.printf("Got 409 on attempt %d, sleeping for %d ms + %d ms and retrying%n", attempt, backoff, jitter);
+        }
+        Thread.sleep(backoff + jitter);
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+        throw new RuntimeException(e);
+      }
+      return retryablePut(idpId, idpRequest, attempt + 1);
+    } else {
+      return result;
+    }
+  }
+}
